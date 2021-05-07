3c78c3cf99bbdff94565ebabfb2b6bd5f7eb3f06
Sandeep <is.sandeep7@gmail.com>
2021-05-07 18:28:04 +0530

    Added few java programs

---
+++ src/JavaBasicPrograms/ReverseWordsInAString.java
@@ -0,0 +1,18 @@
+package JavaBasicPrograms;
+
+public class ReverseWordsInAString {
+    // Java program to reverse a string
+    // s = input()
+    public static void main(String[] args) {
+        String s[] = "i like this program very much".
+                split(" ");
+        String ans = "";
+        for (int i = s.length - 1; i >= 0; i--) {
+            ans += s[i] + " ";
+        }
+        System.out.println("Reversed String:");
+        System.out.println(ans.substring(0,
+                ans.length() - 1));
+    }
+}
+
