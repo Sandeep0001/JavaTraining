0142b93d1cc71660681ad5f90a05ba1a912e10f5
Sandeep <is.sandeep7@gmail.com>
2021-05-07 15:20:03 +0530

    print 1 to 100 numbers

---
+++ src/JavaBasicPrograms/Print1to100NumbersWithoutUsingNumbersInCode.java
@@ -0,0 +1,13 @@
+package JavaBasicPrograms;
+
+public class Print1to100NumbersWithoutUsingNumbersInCode {
+
+    public static void main(String[] args){
+        int one = 'A'/'A';
+        String s1 = "asdfghjklp";
+
+        for(int i=one;i<=(s1.length() * s1.length());i++){
+            System.out.println(i);
+        }
+    }
+}
