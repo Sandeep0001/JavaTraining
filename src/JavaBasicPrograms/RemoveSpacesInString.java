f6d2ffbbea905b4ef0297b7d93c842f3672acbfa
Sandeep <is.sandeep7@gmail.com>
2021-05-07 18:23:34 +0530

    Added few java programs

---
+++ src/JavaBasicPrograms/RemoveSpacesInString.java
@@ -0,0 +1,17 @@
+package JavaBasicPrograms;
+
+public class RemoveSpacesInString {
+
+    // Function to remove all
+    // spaces from a given string
+    static String removeSpace(String str) {
+        str = str.replaceAll("\\s", "");
+        return str;
+    }
+
+    // Driver Code
+    public static void main(String args[]) {
+        String str = "g eeks for ge eeks ";
+        System.out.println(removeSpace(str));
+    }
+}
