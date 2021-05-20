2fa3fbde7631557cacf6dfe0341b660140a59e1a
Sandeep <is.sandeep7@gmail.com>
2021-05-20 18:31:02 +0530

    programs

---
+++ src/JavaBasicPrograms/StringQuotes.java
@@ -0,0 +1,36 @@
+package JavaBasicPrograms;
+
+public class StringQuotes {
+
+    /**
+     *   /"Hello"/
+     *   /'Hello'/
+     *   "Hello"
+     *   I love "Java" and "Programming"
+     *   'I love "Java" and "Programming"'
+     *
+     * */
+
+
+    public static void main(String[] args) {
+
+        System.out.println("/\"Hello\"/");
+        System.out.println("/'Hello'/");
+        System.out.println("\"Hello\"");
+        System.out.println("'I love \"Java\" and \"Programming\"'");
+
+
+        System.out.println(getXpath("Sandeep")); ////input[@id='Sandeep']
+
+    }
+
+    public static String getXpath(String name){
+
+        String xpath = "//input[@id='"+name+"']";
+
+        return xpath;
+
+    }
+
+
+}
---
+++ src/JavaBasicPrograms/TestCode.java
@@ -0,0 +1,28 @@
+package JavaBasicPrograms;
+
+public class TestCode {
+
+    /**
+     * what will be the output when you print this?
+     * (byte) + (char) - (int) + (long) -1
+     * */
+
+    public static void main(String[] args) {
+
+        int i = (byte) + (char) - (int) + (long) -1;
+        System.out.println(i); //1 --> it casts long of -1 and execution starts right to left and hence the output 1
+
+        int j = (int) + (long) -1;
+        System.out.println(j);//-1
+
+        int k = (char) 1;
+        System.out.println(k); //1
+
+        int m = (byte) 1;
+        System.out.println(m); //1
+
+
+
+    }
+
+}
