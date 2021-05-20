3fa841129baf19285fa871d8ba37da2f6c89671b
Sandeep <is.sandeep7@gmail.com>
2021-05-20 16:08:38 +0530

    added pattern and string programs

---
+++ src/JavaBasicPrograms/Patterns/StarPattern2.java
@@ -0,0 +1,45 @@
+package JavaBasicPrograms.Patterns;
+
+public class StarPattern2 {
+
+    public static void main(String[] args) {
+
+        for (int i=0; i<=4; i++){
+            for(int j=4;j>=i;j--){
+                System.out.print(" ");
+            }
+            for (int k=0;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+            /**
+             *      *
+             *     **
+             *    ***
+             *   ****
+             *  ***** */
+        }
+
+
+        System.out.println("--------------------------------------------");
+
+        for (int l=0;l<=3;l++){
+            for(int m=3;m>=l;m--){
+                System.out.print(" ");
+            }
+            for (int n=0;n<=l;n++){
+                System.out.print(" *");
+            }
+            System.out.println();
+            /**
+             *     *
+             *    * *
+             *   * * *
+             *  * * * *   */
+        }
+
+
+
+    }
+
+}
