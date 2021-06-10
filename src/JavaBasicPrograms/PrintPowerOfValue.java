b546bdb698cc7a5841ef4133f47a27c2f9e328f0
Sandeep <is.sandeep7@gmail.com>
2021-06-10 13:58:02 +0530

    programs

---
+++ src/JavaBasicPrograms/PrintPowerOfValue.java
@@ -0,0 +1,29 @@
+package JavaBasicPrograms;
+
+public class PrintPowerOfValue {
+
+    static double pow(int a, int b){
+
+        if(b==0){
+            return 1;
+        }
+
+        if(b>0) {
+            return a * pow(a, b - 1);
+        }else{
+            double d = 1.0 / (double)a * pow(a, b+1);
+            return d;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(pow(2, 4)); //16
+
+        System.out.println(pow(3,-3)); //0.037037037037037035
+
+    }
+
+
+}
