064449ff3698b8ea4c933f73316ee448d09f6920
sandeep0001 <is.sandeep7@gmail.com>
2023-10-09 15:57:38 +0530

    Find if sum exists in an array program

---
+++ src/programs/FindIfSumExistsInArray.java
@@ -0,0 +1,21 @@
+package programs;
+
+import java.util.HashSet;
+
+public class FindIfSumExistsInArray {
+
+    public static boolean hasSum(int[] arr, int expectedSum){
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : arr){
+            if (set.contains(expectedSum - i)){
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        System.out.println(hasSum(new int[]{2,3,4,5,6,7,8,9}, 16));
+    }
+}
